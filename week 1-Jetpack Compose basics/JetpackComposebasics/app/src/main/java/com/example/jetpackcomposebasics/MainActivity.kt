afb7f10f606190f7ccd5cbb6b0fdc188ae0670ee
Derrick Kim <59466342+thlbyl0109@users.noreply.github.com>
2021-11-02 09:41:16 +0900

    <practice> 첫째주 시작! Compose
    - 구성 가능한 함수 정의
    - 컴포저블에 다른 요소 추가
    - 레이아웃 컴포저블을 사용하여 UI 구성요소 구조화
    - 수정자를 사용한 컴포저블 확장
    - 효율적인 목록 만들기
    - 상태 추적 유지 및 수정
    - 컴포저블에 사용자 상호작용 추가
    - 메시지 확장 중 메시지에 애니메이션 적용
    - 이러한 단계 중 일부에 관해 자세히 알아보려면 아래 리소스를 살펴보세요.

---
+++ week 1-Jetpack Compose basics/JetpackComposebasics/app/src/main/java/com/example/jetpackcomposebasics/MainActivity.kt
@@ -0,0 +1,127 @@
+package com.example.jetpackcomposebasics
+
+import android.content.res.Configuration
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.jetpackcomposebasics.ui.theme.JetpackComposeBasicsTheme
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetpackComposeBasicsTheme {
+                // A surface container using the 'background' color from the theme
+//                Surface(color = MaterialTheme.colors.background) {
+//
+//                }
+                Conversation(SampleData.conversationSample)
+            }
+        }
+    }
+}
+data class Message(val author: String, val body: String)
+
+@Composable
+fun MessageCard(msg : Message) {
+//    Text(text = "Hello $name!")
+    // Add padding around our message
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        Image(painter = painterResource(R.drawable.profile_picture),
+            contentDescription = "Contact profile picture",
+            modifier = Modifier
+                // Set image size to 40 dp
+                .size(40.dp)
+                // Clip image to be shaped as a circle
+                .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
+        )
+        // Add a horizontal space between the image and the column
+        Spacer(modifier = Modifier.width(8.dp))
+        // We keep track if the message is expanded or not in this
+        // variable
+        var isExpanded by remember { mutableStateOf(false) }
+        // surfaceColor will be updated gradually from one color to the other
+        val surfaceColor: Color by animateColorAsState(
+            if (isExpanded) MaterialTheme.colors.primary else MaterialTheme.colors.surface,
+        )
+        // We toggle the isExpanded variable when we click on this Column
+        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
+            Text(text = msg.author,
+                color = MaterialTheme.colors.secondaryVariant,
+                style = MaterialTheme.typography.subtitle2
+            )
+            // Add a vertical space between the author and message texts
+            Spacer(modifier = Modifier.height(4.dp))
+
+            Surface(shape = MaterialTheme.shapes.medium,
+                elevation = 1.dp)
+            {
+                Text(
+                    text = msg.body,
+                    modifier = Modifier.padding(all = 4.dp),
+                    // If the message is expanded, we display all its content
+                    // otherwise we only display the first line
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
+                    style = MaterialTheme.typography.body2
+                )
+            }
+        }
+    }
+
+}
+@Composable
+fun Conversation(messages: List<Message>) {
+    LazyColumn {
+        items(messages) { message ->
+            MessageCard(message)
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewConversation() {
+    JetpackComposeBasicsTheme {
+        Conversation(SampleData.conversationSample)
+    }
+}
+
+@Preview(
+    name = "Light Mode",
+    uiMode = Configuration.UI_MODE_NIGHT_NO,
+    showBackground = true
+)
+@Preview(
+    name = "Dark Mode",
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true
+)
+@Composable
+fun PreviewMessageCard() {
+    JetpackComposeBasicsTheme {
+//        MessageCard(name = "Android")
+        MessageCard(
+            msg = Message("Android", "Jetpack Compose")
+        )
+    }
+}
